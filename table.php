da46d1e67a2f25299c0fedd014a7f3806493e457
kreestinaa <bhandarikristina043@gmail.com>
2025-02-05 21:12:55 +0545

    Create table.php
    table

---
+++ table.php
@@ -0,0 +1,22 @@
+</head>
+<body>
+<TABLE ALIGN="center" WIDTH="400" HEIGHT="500" border="1">
+    <TR>
+        <TD colspan="2" rowspan="1">header</TD>
+    </TR>
+    <TR>
+        <TD colspan="2" rowspan="1">nav</TD>
+    </TR>
+    <TR>
+        <TD colspan="1" rowspan="1">section</TD>
+        <TD colspan="1" rowspan="2">aside</TD>
+    </TR>
+    <TR>
+        <TD colspan="1" rowspan="1">article</TD>
+    </TR>
+    <TR>
+        <TD colspan="2" rowspan="1">footer</TD>
+    </TR>
+</TABLE>
+</body>
+</html>
