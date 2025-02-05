ec8ddfb1a5edb30b95e07a1484d448aed01b2555
kreestinaa <bhandarikristina043@gmail.com>
2025-02-05 21:12:36 +0545

    Create element.php
    elements

---
+++ element.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<B>Bold</B><BR>
+ <I>Italic</I><BR>
+ <TT>Teletype (Monospaced)</TT><BR>
+ <U>Underlined</U><BR>
+ Subscripts: f<SUB>0</SUB> + f<SUB>1</SUB><BR>
+ Superscripts: x<SUP>2</SUP> + y<SUP>2</SUP><BR>
+ <SMALL>Smaller</SMALL><BR>
+ <BIG>Bigger</BIG><BR>
+ <STRIKE>Strike Through</STRIKE><BR>
+ <B><I>Bold Italic</I></B><BR>
+ <BIG><TT>Big Monospaced</TT></BIG><BR>
+ <SMALL><I>Small Italic</I></SMALL><BR>
+ <FONT COLOR="GRAY">Gray</FONT><BR>
+ <DEL>Delete</DEL><BR>
+ <INS>Insert</INS><BR>
+</body>
+</html>
